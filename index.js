c8a47ebd800d080948ebaa5b43b111579d36d35c
hasan <akonhasan680@gmail.com>
2023-05-20 02:12:37 +0600

    post data implement

--- index.js
+++ index.js
@@ -1,20 +1,76 @@
 const express = require('express');
 const cors = require('cors');
-const app=express()
-const  port=process.env.PORT||5000;
-const imgData =require('./imageData.json')
+const app = express()
+const port = process.env.PORT || 5000;
+const imgData = require('./imageData.json')
 app.use(cors());
 require('dotenv').config()
 app.use(express.json())
+// toys Shop 
+// eUDFPWMFuWaCTOey
+console.log(process.env.DB_user)
+const {
+  MongoClient,
+  ServerApiVersion
+} = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.ytnqryr.mongodb.net/?retryWrites=true&w=majority`;
 
-
-
-app.get('/imgData',(req,res)=>{
-res.send(imgData);
-})
-app.get('/',(req,res)=>{
-    res.send('server running')
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
-app.listen(port,()=>{
-console.log(`server is running on port ${port}`)
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const toysCollection = client.db('CarsToy').collection('cars');
+    app.post('/addtoys',async(req, res) => {
+      const body = req.body;
+      const result=await toysCollection.insertOne(body);
+      res.send(result);
+
+    })
+
+    app.get('/data/:category', async(req,res)=>{
+      const category=req.params.category;
+      if(category=='Vintage Cars'|| category=='Off-Road Vehicles'||category=='Sports Cars'){
+        const result=await toysCollection.find({subcategory:category}).toArray();
+        res.send(result)
+      }
+      else{
+        res.send({massage:"category data not found"})
+      }
+
+    })
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({
+      ping: 1
+    });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/imgData', (req, res) => {
+  res.send(imgData);
+})
+app.get('/', (req, res) => {
+  res.send('server running')
+});
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`)
 })
